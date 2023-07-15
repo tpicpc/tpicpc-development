a00d8b52261e1da82f630a8f5e0555f75b940f09
Sahinur Islam <infosahinur@gmail.com>
2023-07-15 17:26:00 +0600

    [FIX] Head

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -2,9 +2,8 @@ import "../../styles/globals.css";
 import "../../styles/custom.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import { Metadata } from 'next'
 
-export const metadata:Metadata = {
+export const metadata = {
   title: "TPI - Computer Programming Club",
   description: "Join TPI - Computer Programming Club to learn and explore the world of coding. Our experienced instructors will guide you through the fundamentals of programming languages such as Java, Python, and C++. Whether you're a beginner looking to get started or a seasoned programmer looking to expand your skillset, our club has something for everyone. With hands-on projects, hackathons, and workshops, you'll have the opportunity to put your skills to the test and develop your knowledge in a collaborative and supportive environment. Join us today and take your programming skills to the next level!",
   keywords: "TPI,TPICPC,Computer Programming Club,Programming Club,Programming,Java,Python,C++,Coding,Learn,Explore,Instructors,Workshops,Hackathons,Projects,Hands-on,Skills,Knowledge,Supportive Environment,Join,Next Level",
