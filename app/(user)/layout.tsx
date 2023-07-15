a4283b0b44758b4d55f30d70a6cd6265a80ffb28
Sahinur Islam <infosahinur@gmail.com>
2023-07-15 17:06:14 +0600

    [FIX] Error

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html>
+    <html lang="en" suppressHydrationWarning={true}>
       <head />
       <body>
         <Header />
